f3ba17bc0e6895cc6948a576e687ec4663631da6
Nina <kormina.nn@gmail.com>
2026-01-14 13:06:58 +0000

    first commit

---
+++ js/script.js
@@ -0,0 +1,15 @@
+"use strict";
+
+console.log("arr" + "-object");
+console.log(4 + +"5");
+
+let awe = 10,
+    wea = 10;
+
+// ++awe;
+// --wea;
+
+console.log(++awe);
+console.log(--wea);
+
+console.log(5%2);
